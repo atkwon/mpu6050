53a896b93d4a3e650ea442ccde70f8cbc89b18b2
atkwon <atkwon20@gmail.com>
2025-03-07 11:39:25 -0500

    create structs for handle and config to manage i2c instances

--- mpu6050.h
+++ mpu6050.h
@@ -13,20 +13,6 @@
 #define MPU6050_GYRO_XOUT  0x43
 #define MPU6050_ACCEL_XOUT 0x3B
 
-/*
- * Gyro Full Scale      / Sensitivity
- * 0 => 250                     131
- * 1 => 500                     65.5
- * 2 => 1000                    32.8
- * 3 => 2000 deg/s              16.4 LSB/(deg/s)
- *
- * Accel Full Scale     / Sensitivity
- * 0 => 2                       16384
- * 1 => 4                       8192
- * 2 => 8                       4096
- * 3 => 16g                     2048 LSB/g
- */
-
 #define GYRO_FULL_SCALE_250	131f
 #define GYRO_FULL_SCALE_500	65.5f
 #define GYRO_FULL_SCALE_1000	32.8f
@@ -37,16 +23,30 @@
 #define ACCEL_FULL_SCALE_8	4096
 #define ACCEL_FULL_SCALE_16	2048
 
+typedef struct{
+	i2c_inst_t *	i2c;
+	uint8_t 	address;
+} mpu6050_handle_t;
 
-int mpu6050_init(i2c_inst_t *i2c,
-	uint		      sda,
-	uint		      scl,
-	uint8_t		      gyro_fs,
-	uint8_t		      accel_fs,
-	uint		      baudrate);
+typedef struct{
+	uint		sda;
+	uint		scl;
+	uint8_t		gyro_fs;
+	uint8_t		accel_fs;
+	uint		baudrate;
+} mpu6050_config_t;
 
-int mpu6050_read_gyro(int16_t *gx, int16_t *gy, int16_t *gz);
+typedef struct{
+	int16_t 	x;
+	int16_t 	y;
+	int16_5 	z;
+} mpu6050_axis_t;
 
-int mpu6050_read_accel(int16_t *ax, int16_t *ay, int16_t *az);
+
+int mpu6050_init(mpu6050_handle_t *handle, mpu6050_config_t *config);
+
+int mpu6050_read_gyro(mpu6050_handle_t *handle, mpu6050_axis_t *gyro);
+
+int mpu6050_read_accel(mpu6050_handle_t *handle, mpu6050_axis_t *accel);
 
 #endif /* MPU6050_H */
