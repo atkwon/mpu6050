be3aff41b1faf1bc615c103e091a5e1a09ad91de
atkwon <atkwon20@gmail.com>
2025-03-19 05:59:36 -0400

    create bit masks for config

---
+++ reg.h
@@ -0,0 +1,35 @@
+#define mask2shift(X) __builtin_ctzg(X)
+
+#define bit_set(X, mask) (((X) << mask2shift(mask)) & (mask))
+#define bit_get(X, mask) (((X) & (mask)) >> mask2shift(mask))
+
+#define MPU_6050_GYRO_CONFIG 0x1B
+#define MPU_6050_ACCEL_CONFIG 0x1C
+
+#define MPU_6050_GYRO_CONFIG_XG_ST_MASK  (0x1 << 7)
+#define MPU_6050_GYRO_CONFIG_YG_ST_MASK  (0x1 << 6)
+#define MPU_6050_GYRO_CONFIG_ZG_ST_MASK  (0x1 << 5)
+#define MPU_6050_GYRO_CONFIG_FS_SEL_MASK (0x3 << 3)
+
+#define MPU_6050_ACCEL_CONFIG_XG_ST_MASK  (0x1 << 7)
+#define MPU_6050_ACCEL_CONFIG_YG_ST_MASK  (0x1 << 6)
+#define MPU_6050_ACCEL_CONFIG_ZG_ST_MASK  (0x1 << 5)
+#define MPU_6050_ACCEL_CONFIG_FS_SEL_MASK (0x3 << 3)
+
+#define MPU_6050_PM_CONFIG_SLEEP_MASK	(0x1 << 6)
+#define MPU_6050_PM_CONFIG_CLKSEL_MASK	(0x3 << 3)
+
+enum mpu_6050_gyro_config_fs_sel {
+	MPU_6050_GYRO_CONFIG_FS_SEL_250  = 0,
+	MPU_6050_GYRO_CONFIG_FS_SEL_500  = 1,
+	MPU_6050_GYRO_CONFIG_FS_SEL_1000 = 2,
+	MPU_6050_GYRO_CONFIG_FS_SEL_2000 = 3,
+};
+
+enum mpu_6050_accel_config_fs_sel {
+	MPU_6050_ACCEL_CONFIG_FS_SEL_2	= 0,
+	MPU_6050_ACCEL_CONFIG_FS_SEL_4	= 1,
+	MPU_6050_ACCEL_CONFIG_FS_SEL_8	= 2,
+	MPU_6050_ACCEL_CONFIG_FS_SEL_16	= 3,
+};
+
